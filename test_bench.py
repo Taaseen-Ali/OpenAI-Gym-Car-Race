911f813e05308579eeebc3ac28234f0b0edddf86
Taaseen Ali <taaseen.ali89@gmail.com>
2021-02-17 16:50:54 -0500

    removed reward func

--- test_bench.py
+++ test_bench.py
@@ -7,26 +7,6 @@ from SelfDriveEnv import Car, Track
 from training_utils import TensorboardCallback, constant_schedule, linear_schedule, run_experiment, testing, with_changes
 from config import cfg
 
-# Create custom reward function
-
-@Car.reward_function
-def reward_func(car):
-    reward = 0
-    if car.crashed:
-        reward = car.config["reward"]["crash_reward"]
-    elif car.has_finished:
-        print('finished!')
-        reward += 100
-    else:
-        curr = car.current_tile()
-        if curr not in car.traveled:
-            reward = car.config["reward"]["new_tile_reward"] * (len(car.traveled) + 1)
-        elif car.speed <= car.config["reward"]["min_speed"]:
-            reward = car.config["reward"]["same_tile_penalty"]
-        else:
-            reward = car.config["reward"]["same_tile_reward"]
-    return reward
-
 # Specify folders to save models/logs in
 
 model_dir = "learning-rate-tests"
